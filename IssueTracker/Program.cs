6fe1a2ed6d7e4d0b47be13eb6af9b257d4d25ab8
vp799 <37125650+Veep1@users.noreply.github.com>
2022-07-05 11:30:37 -0400

    Added clickable rows and switched edit and delete to buttons

--- IssueTracker/Program.cs
+++ IssueTracker/Program.cs
@@ -50,7 +50,7 @@ namespace IssueTracker
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Project}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
